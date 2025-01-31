72b2f6c51a9514edfa74fe2b219c037bbf6d4508
Stas Merzyakov <merzliakov@gmail.com>
2025-01-31 15:14:48 +0400

    L01

---
+++ L01-gradle/build.gradle.kts
@@ -0,0 +1,26 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+
+plugins {
+    id("com.github.johnrengelman.shadow")
+}
+
+val guava: String by rootProject.extra
+
+dependencies {
+    implementation("com.google.guava:guava:${guava}")
+}
+
+tasks {
+    named<ShadowJar>("shadowJar") {
+        archiveBaseName.set("gradleHelloWorld")
+        archiveVersion.set("0.1")
+        archiveClassifier.set("")
+        manifest {
+            attributes(mapOf("Main-Class" to "ru.otus.Main"))
+        }
+    }
+
+    build {
+        dependsOn(shadowJar)
+    }
+}
