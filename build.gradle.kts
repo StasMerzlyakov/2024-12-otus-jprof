97f29cb320bd5a32adcaf25816fd17fabc685ac1
Stas Merzlyakov <merzliakov@gmail.com>
2025-02-03 15:10:36 +0400

    переименовал папку с ДЗ, перенес планигы sonar и spotless, таску managedVersion и добавил разрешение конфликтов

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,34 +1,120 @@
+import io.spring.gradle.dependencymanagement.dsl.DependencyManagementExtension
+import name.remal.gradle_plugins.sonarlint.SonarLintExtension
+import org.gradle.plugins.ide.idea.model.IdeaLanguageLevel
+
 plugins {
-    id("java")
+    idea
+    id("io.spring.dependency-management")
+    id("name.remal.sonarlint") apply false
+    id("com.diffplug.spotless") apply false
 }
 
-
-val junit: String by rootProject.extra
+idea {
+    project {
+        languageLevel = IdeaLanguageLevel(21)
+    }
+    module {
+        isDownloadJavadoc = true
+        isDownloadSources = true
+    }
+}
 
 allprojects {
-    plugins.apply(JavaPlugin::class.java)
-    group = "org.example"
-    version = "1.0-SNAPSHOT"
+    group = "ru.otus"
 
     repositories {
+        mavenLocal()
         mavenCentral()
     }
 
-    dependencies {
-        testImplementation(platform("org.junit:junit-bom:${junit}"))
-        testImplementation("org.junit.jupiter:junit-jupiter")
+    val guava: String by rootProject.extra
+    val testcontainersBom: String by rootProject.extra
+    val junit: String by rootProject.extra
+
+    apply(plugin = "io.spring.dependency-management")
+    dependencyManagement {
+        dependencies {
+            imports {
+                mavenBom("org.testcontainers:testcontainers-bom:$testcontainersBom")
+                mavenBom("org.junit:junit-bom:${junit}")
+            }
+            dependency("com.google.guava:guava:$guava")
+        }
     }
 
-    tasks {
-        test {
-            useJUnitPlatform()
-        }
+    configurations.all {
+        resolutionStrategy {
+            failOnVersionConflict()
 
-        extensions.configure<JavaPluginExtension> {
-            sourceCompatibility = JavaVersion.VERSION_21
-            targetCompatibility = JavaVersion.VERSION_21
+            force("javax.servlet:servlet-api:2.5")
+            force("commons-logging:commons-logging:1.1.1")
+            force("commons-lang:commons-lang:2.5")
+            force("org.codehaus.jackson:jackson-core-asl:1.8.8")
+            force("org.codehaus.jackson:jackson-mapper-asl:1.8.8")
+            force("commons-io:commons-io:2.16.1")
+            force("org.eclipse.jgit:org.eclipse.jgit:6.9.0.202403050737-r")
+            force("org.apache.commons:commons-compress:1.26.1")
+            force("com.google.errorprone:error_prone_annotations:2.36.0")
+            force("org.jetbrains:annotations:19.0.0")
+            force("commons-codec:commons-codec:1.16.1")
+            force("org.jetbrains.kotlin:kotlin-stdlib:1.6.10")
+            force("com.google.code.gson:gson:2.11.0")
+            force("org.jetbrains.kotlin:kotlin-stdlib-common:1.6.10")
+            force("org.apache.commons:commons-lang3:3.14.0")
+            force("net.bytebuddy:byte-buddy:1.15.4")
         }
+    }
+}
 
+subprojects {
+    plugins.apply(JavaPlugin::class.java)
+    extensions.configure<JavaPluginExtension> {
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
+    }
+
+    tasks.withType<JavaCompile> {
+        options.encoding = "UTF-8"
+        options.compilerArgs.addAll(listOf("-Xlint:all,-serial,-processing"))
+
+        dependsOn("spotlessApply")
+    }
+
+    apply<name.remal.gradle_plugins.sonarlint.SonarLintPlugin>()
+    configure<SonarLintExtension> {
+        nodeJs {
+            detectNodeJs = false
+            logNodeJsNotFound = false
+        }
+    }
+
+    apply<com.diffplug.gradle.spotless.SpotlessPlugin>()
+    configure<com.diffplug.gradle.spotless.SpotlessExtension> {
+        java {
+            palantirJavaFormat("2.39.0")
+        }
+    }
+
+    tasks.withType<Test> {
+        useJUnitPlatform()
+        testLogging.showExceptions = true
+        reports {
+            junitXml.required.set(true)
+            html.required.set(true)
+        }
+    }
+
+}
+
+tasks {
+    val managedVersions by registering {
+        doLast {
+            project.extensions.getByType<DependencyManagementExtension>()
+                .managedVersions
+                .toSortedMap()
+                .map { "${it.key}:${it.value}" }
+                .forEach(::println)
+        }
     }
 }
 
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,10 +5,17 @@ pluginManagement {
         mavenCentral()
     }
     val johnrengelman: String by settings
+    val sonarlint: String by settings
+    val spotless: String by settings
+    val dependencyManagement: String by settings
 
     plugins {
+        id("io.spring.dependency-management") version dependencyManagement
         id("com.github.johnrengelman.shadow") version johnrengelman
+        id("name.remal.sonarlint") version sonarlint
+        id("com.diffplug.spotless") version spotless
     }
 }
 rootProject.name = "jprof"
 include("hw01-gradle")
+include("hw02-generics")
