e99aeb53928a0840b59ad4ab6d574dcb7b7c0bab
Stanislav Merzlyakov <st.merzlyakov@yandex.ru>
2025-03-01 13:58:14 +0400

    тест на незименяемые поля

--- hw07-structuralPatterns/src/test/java/ru/otus/processor/SwapProcessorTest.java
+++ hw07-structuralPatterns/src/test/java/ru/otus/processor/SwapProcessorTest.java
@@ -26,4 +26,24 @@ public class SwapProcessorTest {
         assertThat(inputMessage.getField11()).isEqualTo(field11);
         assertThat(inputMessage.getField12()).isEqualTo(field12);
     }
+
+    @Test
+    void noChangeTest() {
+        var processor = new SwapProcessor();
+
+        var field9 = "field9";
+        var field10 = "field10";
+        var inputMessage =
+                new Message.Builder(1L).field9(field9).field10(field10).build();
+
+        var outputMessage = processor.process(inputMessage);
+
+        // проверка выходного сообщения
+        assertThat(outputMessage.getField9()).isEqualTo(field9);
+        assertThat(outputMessage.getField10()).isEqualTo(field10);
+
+        // проверка исходного сообщения
+        assertThat(inputMessage.getField9()).isEqualTo(field9);
+        assertThat(inputMessage.getField10()).isEqualTo(field10);
+    }
 }
